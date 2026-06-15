fb616e6afd658ce852e62a96f2a398a4016d1eb6
mahirmalvi <malvimahirm@gmail.com>
2026-06-15 22:58:40 +0530

    Updated CyberShield-AI project

--- app.py
+++ app.py
@@ -657,4 +657,4 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     # Run server locally on default development port 5000
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
